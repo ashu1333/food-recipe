3ce157e970088100203a2e01c7a9deafab952eee
akash <akashverma1333@gmail.com>
2021-03-29 14:43:26 +0530

    food-recipe

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
+
+function App() {
+  const [search, setSerach] = useState("chiken");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "2c00082d";
+  const APP_KEY = "77065fd1badce9b5a356e29a39710c6b";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(res.data.hits);
+  };
+
+  const onInputChange = (e) => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
+  return (
+    <div className="App">
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
